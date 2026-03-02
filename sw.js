b39789784429ebd022d64eec4043461c753adc60
CleberMachado82 <c.cleber.clm@gmail.com>
2026-03-01 21:10:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'financas-v3';
+const CACHE_NAME = 'financas-v4'; // Mudamos para v4 para forçar o update
 const assets = [
   './',
   './index.html',
@@ -9,6 +9,7 @@ const assets = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('Cache v4 instalado com sucesso!');
       return cache.addAll(assets);
     })
   );
